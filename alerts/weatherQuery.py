79b993405ed37d4b342976e7bc4c971ae6b4751c
Kevin <gitOnRaspberryPi@haz-zone.com>
2013-07-31 15:30:50 -0400

    remove expired alerts is working in the test environment.

--- alerts/weatherQuery.py
+++ alerts/weatherQuery.py
@@ -4,7 +4,7 @@
 import urllib2
 import json
 import os.path
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta, tzinfo
 
 #from sys import maxint
 #from printKeys import printKeys
@@ -14,12 +14,12 @@ dev = False
 dev = True
 
 
-#-------------------------------------------------------------------------------
+#===============================================================================
 # File locations
 alertFile = '/var/www/html/dev/info.json'
 keyFile = '../key'
 key = ""
-#-------------------------------------------------------------------------------
+#===============================================================================
 
 
 def timestamp():
@@ -29,7 +29,7 @@ def timestamp():
 print timestamp() + " Alert monitoring system started."
 
 
-def alertCheck(alertId):
+def alertCheck(alertId):#, loc, locQ):
   if os.path.isfile(keyFile) and os.path.getsize(keyFile) > 0:
     try:
       with open(keyFile, 'r') as f:
@@ -46,6 +46,7 @@ def alertCheck(alertId):
   if key != "" and (not dev):
     try:
       response = urllib2.urlopen('http://api.wunderground.com/api/' + str(key) + '/geolookup/conditions/q/zmw:44202.1.99999.json')
+#      response = urllib2.urlopen('http://api.wunderground.com/api/' + str(key) + '/alert' + locQ +  '.json')
       json_string = response.read()
       response.close()
     except:
@@ -61,14 +62,17 @@ def alertCheck(alertId):
       print timestamp() +  " Error using file."
 #-------------------------------------------------------------------------------
 
-# Function for extracting and formatting only the needed information to JSON
-  def formatJsonAlert(a):
+# Function for extracting and formatting only the needed information to JSON.
+# Be very careful with quotes and test any changes thoroughly.
+  def formatJsonAlert(a):#, loc):
     ++alertId
+      #'","location":"' + loc + \
     newAlert = '{"id":' + str(alertId) + \
       ',"type":"' + a['type'] + \
       '","date":"' + a['date'] + \
       '","expires":"' + a['expires'] + \
-      '","description":"' + a['description'] + \
+      '","expires_epoch":' + a['expires_epoch'] + \
+      ',"description":"' + a['description'] + \
       '","message":' + json.dumps(a['message']) + \
       ',"phenomena":"' + a['phenomena'] + \
       '","significance":"' + a['significance'] + '"}'
@@ -102,11 +106,46 @@ def alertCheck(alertId):
         print timestamp() + "Error with information file"
 
 
-  #print json.dumps(parsed_json, sort_keys=True, indent=4, separators=(',', ': '))
-  #printKeys(parsed_json)
-  #print "Current temperature in %s is: %s" % (location, temp_f) 
-  #for l in range(len(r['alerts'])):
-  #  newAlert = formatJsonAlert(r['alerts'][l])
+def expirationCheck():
+  if os.path.isfile(alertFile) and os.path.getsize(alertFile) > 0:
+    try:
+      with open(alertFile, 'r+') as fin:
+        json_string = fin.read()
+    except IOError as e:
+      print timestamp() + " Error with information file"
+    if json_string != "":
+      f = json.loads(json_string)
+      count = len(f)
+      listOffset = 0
+      for alert in range(count):
+        print "alert: " + str(alert)
+        print "listOffset : " + str(listOffset)
+        if datetime.utcfromtimestamp(f[alert - listOffset]['expires_epoch']) < datetime.utcnow():
+          del f[alert - listOffset]
+          listOffset += 1
+          print "deleted"
+      if len(f) < count and len(f) != 0:
+        try:
+          with open(alertFile, 'w') as fout:
+            fout.write(json.dumps(f) + '\n')
+        except IOError as e:
+          print timestamp() + " Error with information file"
+      elif len(f) == 0:
+        try:
+          with open(alertFile, 'w') as fout:
+            fout.write("")
+        except IOError as e:
+          print timestamp() + " Error with information file"
+        
+comment = """
+        print f[alert]['expires_epoch']
+        print datetime.fromtimestamp(f[alert]['expires_epoch'])
+        print datetime.utcfromtimestamp(f[alert]['expires_epoch'])
+        print datetime.today()
+        print datetime.utcnow()
+        #print datetime.fromtimestamp(f[alert]['expires_epoch'], tzinfo.tzname(datetime.today()))
+        print f[alert]['expires']
+"""
 
 
 #-------------------------------------------------------------------------------
@@ -114,7 +153,7 @@ def alertCheck(alertId):
 
 # Program flow:
 # On startup load:
-# if the it is not empty, load the information file storage file
+# if it is not empty, load the information file storage file
 #   only keep logged any alerts that have not expired
 #
 # Once every hour and a half:
@@ -130,15 +169,21 @@ def alertCheck(alertId):
 # Doing this as an always running job or a scheduled task?
 #... I'm thinking just have it infinitly loop is a background process..
 
-comment = """
+# TODO:
+# Finish exception output for error logging.
+###Because of the developer rate limit and the current rate model planned this 
+###would not be able to make requests for multiple locations without blocking. 
+# Finish adding a function that handles calling alertCheck() for every location.
+#     Loads the locations...
+
 def main():
   startTime = datetime.today()
   # 1.5 hours == 90 minutes == 5400 seconds
-  alertInterval = 5400
-  alertTimer = startTime + alertInterval
+  alertInterval = timedelta(minutes=1)#hours=1.5)
+  alertTimer = startTime - alertInterval
   # 5 minutes == 300 seconds, 20 minutes == 1200 seconds
-  expireInterval = 300
-  expirationTimer = startTime + expireInterval
+  expireInterval = timedelta(minutes=2)
+  expirationTimer = startTime - expireInterval
   # Javascript safe, unique identification
   alertId = -2147483647 #-maxint
 
@@ -149,13 +194,26 @@ def main():
   while True == True:
     if (datetime.today() - alertTimer) >= alertInterval:
       alertTimer = datetime.today()
+# Eventually may add a loop to handle finding alerts for multiple locations. 
+      print timestamp() + " Alert check running"# for " + location
       alertCheck(alertId)
-    if (datetime.today() - alertTimer) >= expireInterval:
+    if (datetime.today() - expirationTimer) >= expireInterval:
       expirationTimer = datetime.today()
+      print timestamp() + " Expiration check running"# for " + location
       expirationCheck()
+comment = """
 """
 
 
 if __name__=="__main__":
-  alertCheck(0)
-  #main()
+  #alertCheck(0)
+  main()
+
+
+
+## Scraps ##
+  #print json.dumps(parsed_json, sort_keys=True, indent=4, separators=(',', ': '))
+  #printKeys(parsed_json)
+  #print "Current temperature in %s is: %s" % (location, temp_f) 
+  #for l in range(len(r['alerts'])):
+  #  newAlert = formatJsonAlert(r['alerts'][l])
