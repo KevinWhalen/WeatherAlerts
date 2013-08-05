dc952ed49de11f33ac35b25cb1bf3b467a60ed46
Kevin <gitOnRaspberryPi@haz-zone.com>
2013-08-05 11:43:48 -0400

    duplicate alerts are no longer saved

--- alerts/weatherQuery.py
+++ alerts/weatherQuery.py
@@ -23,8 +23,7 @@
 
 # TODO:
 # Check if an alert in a response already exists in the information file before
-# inserting it. Investigate why this problem did not occur with the development
-# example data file. 
+# inserting it.
 
 
 import urllib2
@@ -45,8 +44,7 @@ dev = False
 alertFile = '/var/www/html/dev/info.json'
 keyFile = '../key'
 key = ""
-loc = "Cleveland"
-locQ = '/q/zmw:44101.1.99999'
+loc = "Test Location"
 locQ = '/q/zmw:73101.1.99999'
 #http://autocomplete.wunderground.com/aq?query=LOCATION&c=US
 #===============================================================================
@@ -87,6 +85,7 @@ def alertCheck(alertId, loc, locQ):
     try:
       with open("../exampleResponses/singleAlert.json", 'r') as f:
       #with open("../exampleResponses/emptyAlert.json", 'r') as f:
+      #with open("../exampleResponses/alerts.json", 'r') as f:
         json_string = f.read()
     except IOError:
       print timestamp() +  " Error using file."
@@ -97,9 +96,9 @@ def alertCheck(alertId, loc, locQ):
 # Be very careful with quotes and test any changes thoroughly.
   def formatJsonAlert(a):#, loc):
     ++alertId
-      #'","location":"' + loc + \
     newAlert = '{"id":' + str(alertId) + \
       ',"type":"' + a['type'] + \
+      '","location":"' + loc + \
       '","date":"' + a['date'] + \
       '","expires":"' + a['expires'] + \
       '","expires_epoch":' + a['expires_epoch'] + \
@@ -113,19 +112,38 @@ def alertCheck(alertId, loc, locQ):
   if json_string != "":
     cleaned_json_string = json_string.replace('/','')
     r = json.loads(cleaned_json_string) #parse json into a python object
+#    print timestamp() + " Alert count: " + str(r['response']['features']['alerts'])
     if 'error' not in r and len(r['alerts']) > 0:
       if os.path.isfile(alertFile) and os.path.getsize(alertFile) > 0:
         try:
           with open(alertFile, 'r+') as f:
-            f.seek(-2, 2) # current: 0, begining: 1, end: 2
-            #print timestamp() + " Alert count: " + str(r['response']['features']['alerts'])
+            # Check response against current alerts filed.
+            file_string = f.read()
+            current = json.loads(file_string)
+            incoming = list()
             for a in r['alerts']:
+              cleared = 0
+              for c in current:
+                if (str(a['type']) != str(c['type'])
+                  or str(a['description']) != str(c['description'])
+                  or str(a['date']) != str(c['date'])
+                  or str(a['expires_epoch']) != str(c['expires_epoch'])):
+                  cleared += 1
+                else:
+                  cleared -= 1
+              if cleared == len(current):
+                if a not in incoming:
+                  incoming.append(a)
+            # Append new alerts to the information file.
+            f.seek(-2, 2) # current: 0, begining: 1, end: 2
+            for a in incoming:
               newAlert = formatJsonAlert(a)
               f.write(',\n' + newAlert)
             f.write(']\n')
         except IOError:
           print timestamp() + " Error with information file"
           return
+      # Set formatting for a new or empty information file.
       elif not os.path.isfile(alertFile) or os.path.getsize(alertFile) == 0:
         try:
           with open(alertFile, 'w+') as f:
@@ -136,8 +154,14 @@ def alertCheck(alertId, loc, locQ):
             f.seek(-2, 2) # Remove trailing comma and newline character
             f.write(']\n')
         except IOError:
-          print timestamp() + "Error with information file"
+          print timestamp() + " Error with information file"
           return
+    elif 'error' in r:
+      print timestamp() + " Error with request"
+      if 'type' in r['error']:
+        print "    " + r['error']['type']
+      if 'description' in r['error']:
+        print "    " + r['error']['description']
 
 
 def expirationCheck():
@@ -209,6 +233,7 @@ def main():
 
 if __name__=="__main__":
   main()
+
 
 
 
